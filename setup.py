e0dc58e718f68323eabadafa7bdbcd83c41bbc48
Rohit Karki <rohit@live.com>
2015-04-27 23:55:23 +0000

    testing deployement1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='tinyerrands',
+setup(name='tinyErrands',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ try:
 except IOError:
     pass
  
-from tinyerrands import app as application
+from tinyErrands import app as application
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
