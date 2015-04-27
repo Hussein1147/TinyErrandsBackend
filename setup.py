cf86b5dc68d727d4335af8ba6f47e9333d2c7beb
Rohit Karki <rohit@live.com>
2015-04-27 23:47:22 +0000

    hello world from flask

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='tinyerrands',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
+     install_requires=['flask==0.10.1','sqlalchemy==0.8.2','flask-sqlalchemy==1.0'],
      )
