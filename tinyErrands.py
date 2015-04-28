aea76da6fefc53a9a56165935524f1f2739495b8
Rohit Karki <rohit@live.com>
2015-04-28 01:01:25 +0000

    adding test user

--- tinyErrands.py
+++ tinyErrands.py
@@ -1,29 +1,45 @@
 from flask import Flask
+import sys,os
+import stripe
+import unicodedata
+from flask import Flask,request,json,Response,jsonify
+# from parse_rest.connection import register, ParseBatcher
+# from parse_rest.datatypes import Object as ParseObject
+# from parse_rest.user import User
+from sqlalchemy import create_engine,update
+from sqlalchemy.orm import sessionmaker
 from tinyErrandsModel import User,Card
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-# app.config.from_pyfile('tinyErrands.cfg')
-# db = SQLAlchemy(app) 
-# class User(db.Model):
-#     __tablename__ = 'user'
-
-#     id = db.Column(db.Integer, primary_key =True)
-#     name =db.Column(db.String(250),nullable= False)
-#     email = db.Column(db.String(250))
-# class Card(db.Model):
-#     __tablename__ = 'card'
-#     id = db.Column(db.Integer,primary_key=True)
-#     CardNumber = db.Column(db.Integer)
-#     expMonth = db.Column(db.Integer)
-#     expYear = db.Column(db.Integer)
-#     cvc = db.Column(db.Integer)
-#     User_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-#     user = db.relationship(User)
-    
- 
+app.config.from_pyfile('tinyErrands.cfg')
+db = SQLAlchemy(app) 
 @app.route('/')
+@app.route('/createUser',methods=['Post'])
+
+def createUser():
+    data = request.get_json(force=True)
+    userName = unicodedata.normalize('NFKD', data['userName']).encode('ascii','ignore')
+    userEmail=unicodedata.normalize('NFKD', data['userEmail']).encode('ascii','ignore')
+    userCardNumber=unicodedata.normalize('NFKD', data['userCardNumber']).encode('ascii','ignore')
+    userExpMonth=unicodedata.normalize('NFKD', data['userExpMonth']).encode('ascii','ignore')
+    userExpYear=unicodedata.normalize('NFKD', data['userExpYear']).encode('ascii','ignore')
+    userCvc=unicodedata.normalize('NFKD', data['userCvc']).encode('ascii','ignore')
+    
+    #create and add User
+    new_person =User(name=userName,email=userEmail)
+    db.session.add(new_person)
+    db.session.commit()
+    #create and add User Card
+    new_person_card = Card(CardNumber=userCardNumber,expMonth=userExpMonth,expYear=userExpYear,cvc=userCvc,user =new_person)
+    
+    db.session.add(new_person_card)
+    db.session.commit()
+    return Response(json.dumps("Success, Created!"))
+    
 @app.route('/hello')
 def index():
     return "Hello from OpenShift"
+
  
 if __name__ == '__main__':
     app.run()
