f36c6d267afce965002553f73a9a61f2f613b91a
Rohit Karki <rohit@live.com>
2015-06-21 05:19:42 +0000

    Testing get all Users

--- tinyErrands.py
+++ tinyErrands.py
@@ -157,7 +157,7 @@ def createUser():
     # userExpMonth=unicodedata.normalize('NFKD', data['userExpMonth']).encode('ascii','ignore')
     # userExpYear=unicodedata.normalize('NFKD', data['userExpYear']).encode('ascii','ignore')
     try:
-        if session.query(User).filter(User.email == userEmail).one() is  None: 
+        if session.query(User).filter(User.email == userEmail).first() is  None: 
             new_person =User(name=userName,email=userEmail,unhashpassword=userPassword)
             session.add(new_person)
             session.commit()
