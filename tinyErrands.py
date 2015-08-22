1d00b1bf5a7e68b94827a9c8440acddae7bf57d2
Rohit Karki <rohit@live.com>
2015-08-22 04:30:13 +0000

    test

--- tinyErrands.py
+++ tinyErrands.py
@@ -278,7 +278,6 @@ def add_post():
     currentUserEmail = unicodedata.normalize('NFKD', data['currentUserEmail']).encode('ascii','ignore')
     post = unicodedata.normalize('NFKD', data['myPost']).encode('ascii','ignore')
     numberOfTask =  unicodedata.normalize('NFKD', data['numberOfTask']).encode('ascii','ignore')
-    
     dueIn = unicodedata.normalize('NFKD', data['dueDate']).encode('ascii','ignore')
     start=  unicodedata.normalize('NFKD', data['startTime']).encode('ascii','ignore')
     startTime = datetime.strptime(start,"%Y-%m-%dT%H:%M:%S +0000")
@@ -289,10 +288,14 @@ def add_post():
     if numberOfTask == 1:
         firstTask = unicodedata.normalize('NFKD', data['firstTask']).encode('ascii','ignore')
         print firstTask
+        print numberOfTask
         p1 = Post(myPost=post, author=currentUser_obj, timestamp=utcnow,dueDate=dueIn,startTime=startTime, task = {'firstTask':firstTask})
     elif numberOfTask == 2:
         firstTask = unicodedata.normalize('NFKD', data['firstTask']).encode('ascii','ignore')
         secondTask = unicodedata.normalize('NFKD', data['secondTask']).encode('ascii','ignore')
+        print firstTask
+        print secondTask
+        print numberOfTask
         p1 = Post(myPost=post, author=currentUser_obj, timestamp=utcnow,
         dueDate=dueIn,startTime=startTime, task = {'firstTask':firstTask ,'secondTask':secondTask})
 
